5109b405a90f6eac271bb707e044e0c97a14d4e5
JackDixon019 <13709@coderacademy.edu.au>
2025-03-21 16:19:16 +1100

    FEAT: added armour model

--- models/armour.py
+++ models/armour.py
@@ -1,4 +1,5 @@
 from init import db, ma
+from marshmallow import fields, validate
 
 class Armour(db.Model):
     __tablename__ = "armour_pieces"
@@ -10,3 +11,10 @@ class Armour(db.Model):
     max_value = db.Column(db.Integer())
 
     skills = db.relationship("ArmourSkills", back_populates="armour", cascade="all, delete")
+
+class ArmourSchema(ma.Schema):
+    skills = fields.Nested("ArmourSkillSchema", only=["skill"])
+    armour_type = fields.String(validate=validate.OneOf(["helmet","chest","arms","waist","legs","talisman"]))
+
+armour_schema = ArmourSchema()
+armour_schemas = ArmourSchema(many=True)
