04439bb8719f546f9f653bf5c4f2688746e11790
JackDixon019 <13709@coderacademy.edu.au>
2025-04-09 14:23:47 +1000

    FEAT: Added Base model_class to SQLAlchemy

--- init.py
+++ init.py
@@ -2,8 +2,9 @@ from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
 from flask_marshmallow import Marshmallow
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import Base
 
-db = SQLAlchemy()
+db = SQLAlchemy(model_class=Base)
 ma = Marshmallow()
 jwt = JWTManager()
 bcrypt = Bcrypt()
